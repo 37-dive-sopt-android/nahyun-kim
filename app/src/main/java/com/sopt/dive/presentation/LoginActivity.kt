82152953d1b0528a9e4b11a105c7f5bc1644609e
Nahyun Kim <hedghog1004@naver.com>
2025-10-17 15:51:12 +0900

    [REF/#2] 패키지 구조 변경

--- app/src/main/java/com/sopt/dive/presentation/LoginActivity.kt
+++ app/src/main/java/com/sopt/dive/presentation/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.sopt.dive
+package com.sopt.dive.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -32,7 +32,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.sopt.dive.ui.theme.DiveTheme
+import com.sopt.dive.R
+import com.sopt.dive.core.designsystem.theme.DiveTheme
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
