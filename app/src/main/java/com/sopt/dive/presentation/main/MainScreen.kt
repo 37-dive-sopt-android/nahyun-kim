ae243368ab7b1a5e328e0dfdad78a118f09602dc
Nahyun Kim <hedghog1004@naver.com>
2025-10-30 09:58:31 +0900

    [DESIGN/#2] 메인 화면 프로필 추가, 디자인 수정

--- app/src/main/java/com/sopt/dive/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sopt/dive/presentation/main/MainScreen.kt
@@ -1,28 +1,50 @@
 package com.sopt.dive.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.dive.R
+import com.sopt.dive.core.designsystem.theme.DiveTheme
 import com.sopt.dive.presentation.auth.model.RegisterInfo
 
 @Composable
 fun MainScreen(
     resultUserInfo: RegisterInfo,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(20.dp),
         modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 12.dp)
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_profile_dummy),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(100.dp)
+                .clip(RoundedCornerShape(30.dp))
+        )
+
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(10.dp)
@@ -70,5 +92,19 @@ fun MainScreen(
                 fontSize = 24.sp,
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainScreenPreview() {
+    DiveTheme {
+        MainScreen(
+            resultUserInfo = RegisterInfo(
+                id = "nahyun",
+                password = "password",
+                nickname = "작나"
+            ),
+        )
     }
 }
