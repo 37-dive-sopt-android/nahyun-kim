26ce91780dda9371aab4f0cb124a023123fe53d0
Nahyun Kim <hedghog1004@naver.com>
2025-11-07 13:58:33 +0900

    chore: 뷰모델 의존성 추가
    Related to: #6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.kotlinx.immutable)
+    implementation(libs.androidx.lifecycle.viewmodel.compose.android)
 }
